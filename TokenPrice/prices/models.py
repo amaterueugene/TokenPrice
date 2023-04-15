72a91dd488b23465621b6f7d7052649a736c861e
Eugene <tatsuke.private@gmail.com>
2023-04-15 10:40:58 +0300

    Added Token model

--- TokenPrice/prices/models.py
+++ TokenPrice/prices/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Token(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name')
+    short_name = models.CharField(max_length=100, verbose_name='ShortName')
+    price = models.FloatField(verbose_name='TokenPrice')
+    change_day = models.FloatField(verbose_name='24hChange')
+    volume_day = models.FloatField(verbose_name='24hVolume')
+    market_cap = models.FloatField(verbose_name='MarketCap')
