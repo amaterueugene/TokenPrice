721adda490181b1dcf2e52c66a83a5814678560f
Eugene <tatsuke.private@gmail.com>
2023-04-15 11:04:38 +0300

    Created basic ListView, static and templates

--- TokenPrice/prices/views.py
+++ TokenPrice/prices/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Token
 
 
 class TokenPriceList(ListView):
-    pass
+    model = Token
+    template_name = 'prices/token_price_list.html'
+    context_object_name = 'tokens'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = ' - Prices'
+        return context
+    
