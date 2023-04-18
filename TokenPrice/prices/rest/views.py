13d0a7f1a5e4664693b8404a8085e79e5e400d21
Eugene <tatsuke.private@gmail.com>
2023-04-18 12:28:18 +0300

    Added API GET request

---
+++ TokenPrice/prices/rest/views.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TokenSerializer
+from prices.models import Token
+
+class TokenAPIView(APIView):
+     def get(self, request):
+        token = Token.objects.all()
+        return Response({'tokens': TokenSerializer(token, many=True).data})
--- TokenPrice/prices/urls.py
+++ TokenPrice/prices/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from .views import TokenPriceList
+from .rest.views import TokenAPIView
 
 urlpatterns = [
-    path('', TokenPriceList.as_view(), name='PriceListPage')
+    path('', TokenPriceList.as_view(), name='PriceListPage'),
+    #API
+    path('api/v1/', TokenAPIView.as_view(), name='PriceListAPI'),
 ]
